3d5333176908e878c295f077e22c0fed01e703dc
veronicasilva20 <santinoveronicasilva@gmail.com>
2023-05-16 15:35:57 -0300

    agrego  practica de javascript

---
+++ main.js
@@ -0,0 +1,62 @@
+// var amigos =['veronica','bruno','romina', 10, 1, 1000];
+// console.log(amigos[5]);
+
+// var nombres=['alberto','raul'];
+// var edades=[45.87];
+
+// var eda=80
+// var eda2=45
+
+// console.log(eda)
+// if (eda>50){
+//     console.log('es mayor')
+// }
+
+// console.log(eda2)
+
+// if (eda2<50){
+//     console.log('es menor')
+// }
+
+// var edad = prompt('ingresar tu edad')
+// console.log(edad);
+
+// if (edad>18) {
+//     console.log('es mayor');
+    
+// }
+
+// let contador = 3;
+
+// while (contador < 5) {
+//   console.log('El contador es: ' + contador);
+//   contador++;
+// }
+
+// console.log('Fin del bucle');
+
+// let num;
+
+// do {
+//   num = parseInt(prompt('Ingresar número'));
+// } while (num !== 30);
+
+// console.log('El número es igual a ' + num);
+
+
+
+    // var num1 = parseInt(prompt('ingresar nota1'));
+    // var num2 = parseInt(prompt('ingresar nota2'));
+    // var num3 = parseInt(prompt('ingresar nota3'));
+
+    // // console.log(num1);
+    // // console.log(num2);
+    // // console.log(num3);
+    
+    
+
+    // var suma = num1+num2+mum
+
+    // console.log('nota final del trimestre');
+
+
